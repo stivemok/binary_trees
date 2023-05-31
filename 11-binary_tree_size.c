f12d86300a267d8d6d96c75b6e8466aba9d69e6f
nebihadilsebo <shegolenur@hotmail.com>
2023-05-31 05:01:27 -0700

    binary_trees

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -6,9 +6,9 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t size = 0, r 0, l = 0;
+	size_t size = 0, r = 0, l = 0;
 
-	if (tree== NULL)
+	if (tree == NULL)
 	{
 		return (0);
 	}
@@ -16,7 +16,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	{
 		l = binary_tree_size(tree->left);
 		r = binary_tree_size(tree->right);
-		size = r +l + 1;
+		size = r + l + 1;
 
 	}
 	return (size);
