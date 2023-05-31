7225da314125d3a7bcfc00e4739aed4eb17916b6
nebihadilsebo <shegolenur@hotmail.com>
2023-05-31 15:17:54 -0700

    binary_trees

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -24,9 +24,14 @@ int binary_tree_is_full(const binary_tree_t *tree)
 			}
 			return (1);
 		}
+		else if (!tree->left && !tree->right)
+		{
+			return (1);
+		}
 		else
 		{
 			return (0);
 		}
+
 	}
 }
