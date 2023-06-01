c7432a6c281da7062c6a0f0b3c496b10b334e517
nebihadilsebo <shegolenur@hotmail.com>
2023-06-01 04:26:36 -0700

    binary_trees

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -4,53 +4,48 @@
  * @tree: tree to check
  * Return: 0if is not perfect or other number that is the level of height
  */
-int tree_is_perfect(const binary_tree_t *tree);
+int tree_is_perfect(const binary_tree_t *tree)
 {
 	int l = 0, r = 0;
 
 	if (tree->left && tree->right)
 
 	{
-		int l = 0, r = 0;
-
-		if (tree->left && tree->right)
-		{
-			l = 1 + tree_is_perfect(tree->left);
-			r = 1 + tree_is_perfect(tree->right);
-			if (r == 1 && 1 != 0)
-				return (r);
-			return (0);
-		}
-		elseif(!tree->left && !tree->right)
-		{
-			return (1);
-		}
-		else
-		{
-			return (0);
-		}
+		l = 1 + tree_is_perfect(tree->left);
+		r = 1 + tree_is_perfect(tree->right);
+		if (r == l && l != 0)
+			return (r);
+		return (0);
 	}
+	else if (!tree->left && !tree->right)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
 
 /**
  * binary_tree_is_perfect - function that checks if a binary tree is perfect
  * @tree: tree to check
  * Return: 1 if it is or 0 if not
  */
-	int binary_tree_is_perfect(const binary_tree_t *tree)
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int result = 0;
+	if (tree == NULL)
 	{
-		int result = 0;
-
-		if (tree == NULL)
-		{
-			return (0);
-		}
-		else
-		{
-			result = tree_is_perfect(tree);
-			if (result != 0)
-			{
-				return (1);
-			}
-			return (0);
-		}
+		return (0);
 	}
+	else
+	{
+		result = tree_is_perfect(tree);
+		if (result != 0)
+		{
+			return (1);
+		}
+		return (0);
+	}
+}
